cdb95575ff79b44e14dd0913dbbac598c9d9ce5b
Brunno Castigrini <brunno.castigrini>
2021-08-12 13:49:13 -0300

    teste

---
+++ SegundaTask - cópia/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  SegundaTask
+//
+//  Created by Brunno Castigrini on 12/08/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+- (IBAction)goBackButton:(UIButton *)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
